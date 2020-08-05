6322261435dfc361d11e6b7a3af0918096c75358
Parav Pandit <parav@nvidia.com>
2020-08-05 03:56:29 -0500

    mmtracker: Update for kmalloc, kfrree tracking
    Signed-off-by: Parav Pandit <parav@mellanox.com>

--- mm_tracker/trace_parser.go
+++ mm_tracker/trace_parser.go
@@ -8,16 +8,24 @@ import (
 )
 
 type MemEntry struct {
-	call_type   string
-	order       uint64
-	pfn         uint64
+	call_type string
+	order     uint64
+	pfn       uint64 /* useful only for mm */
+
+	/* kmalloc/free friends start */
+	bytes_requested uint64
+	bytes_allocated uint64
+	ptr             uint64
+	/* kmalloc/free friends end */
+
+	call_site   string
 	line_number int
+
 	// pointer to freed entry to link to
 	freeEntry *MemEntry
 }
 
 type PfnTracker struct {
-	name        string
 	alloc_bytes uint64
 	free_bytes  uint64
 	entries     []*MemEntry
@@ -27,6 +35,13 @@ type PfnTracker struct {
 	 * again.
 	 */
 	pfnmap_balanced map[uint64][]*MemEntry
+}
+
+type KmemTracker struct {
+	alloc_bytes uint64
+	free_bytes  uint64
+	entries     []*MemEntry
+	kmemmap     map[uint64]*MemEntry /* allocated addr to entry map */
 }
 
 func get_uint64_value_of_key(kv string, key string, base int) (uint64, error) {
@@ -58,7 +73,7 @@ func get_mm_pfn(possible_kv string) (uint64, error) {
 	return value, err
 }
 
-func decode_pfn_order(entry *MemEntry, valid_kv []string) (error) {
+func decode_pfn_order(entry *MemEntry, valid_kv []string) error {
 	//fmt.Printf("%s %s\n", valid_kv[2], valid_kv[3])
 	pfn, pfn_err := get_mm_pfn(valid_kv[2])
 	if pfn_err != nil {
@@ -70,6 +85,52 @@ func decode_pfn_order(entry *MemEntry, valid_kv []string) (error) {
 	}
 	entry.pfn = pfn
 	entry.order = order
+	return nil
+}
+
+func get_req_bytes(possible_kv string) (uint64, error) {
+
+	value, err := get_uint64_value_of_key(possible_kv, "bytes_req=", 10)
+	return value, err
+}
+
+func get_alloc_bytes(possible_kv string) (uint64, error) {
+
+	value, err := get_uint64_value_of_key(possible_kv, "bytes_alloc=", 10)
+	return value, err
+}
+
+func get_mem_ptr(possible_kv string) (uint64, error) {
+
+	value, err := get_uint64_value_of_key(possible_kv, "ptr=", 16)
+	return value, err
+}
+
+func decode_alloc(entry *MemEntry, valid_kv []string) error {
+	mem_ptr, err := get_mem_ptr(valid_kv[2])
+	if err != nil {
+		return err
+	}
+	bytes_req, req_err := get_req_bytes(valid_kv[3])
+	if req_err != nil {
+		return req_err
+	}
+	bytes_alloc, alloc_err := get_alloc_bytes(valid_kv[4])
+	if alloc_err != nil {
+		return alloc_err
+	}
+	entry.ptr = mem_ptr
+	entry.bytes_requested = bytes_req
+	entry.bytes_allocated = bytes_alloc
+	return nil
+}
+
+func decode_free(entry *MemEntry, valid_kv []string) error {
+	free_ptr, err := get_mem_ptr(valid_kv[2])
+	if err != nil {
+		return err
+	}
+	entry.ptr = free_ptr
 	return nil
 }
 
@@ -105,23 +166,18 @@ func parseLine(line string, line_number int) (*MemEntry, error) {
 	//fmt.Printf("%s ", memEntry.call_type)
 
 	switch memEntry.call_type {
-	case "mm_page_alloc":
+	case "mm_page_alloc", "mm_page_alloc_zone_locked", "mm_page_free", "mm_page_free_batched":
 		err := decode_pfn_order(memEntry, valid_kv)
 		if err != nil {
 			return nil, err
 		}
-	case "mm_page_alloc_zone_locked":
-		err := decode_pfn_order(memEntry, valid_kv)
+	case "kmem_cache_alloc", "kmalloc_node", "kmalloc":
+		err := decode_alloc(memEntry, valid_kv)
 		if err != nil {
 			return nil, err
 		}
-	case "mm_page_free":
-		err := decode_pfn_order(memEntry, valid_kv)
-		if err != nil {
-			return nil, err
-		}
-	case "mm_page_free_batched":
-		err := decode_pfn_order(memEntry, valid_kv)
+	case "kmem_cache_free", "kfree":
+		err := decode_free(memEntry, valid_kv)
 		if err != nil {
 			return nil, err
 		}
@@ -129,13 +185,16 @@ func parseLine(line string, line_number int) (*MemEntry, error) {
 	return memEntry, nil
 }
 
-func order_to_bytes(order uint64) (uint64) {
+func order_to_bytes(order uint64) uint64 {
 	return (order + 1) * 4096
 }
 
 func parse_mm_entries(trace_file string) {
 	pfn_tracker := new(PfnTracker)
 	pfn_tracker.pfnmap = make(map[uint64]*MemEntry)
+
+	kmem_tracker := new(KmemTracker)
+	kmem_tracker.kmemmap = make(map[uint64]*MemEntry)
 
 	data, err := ioutil.ReadFile(trace_file)
 	if err != nil {
@@ -154,24 +213,34 @@ func parse_mm_entries(trace_file string) {
 		}
 
 		switch entry.call_type {
-		case "mm_page_alloc":
+		case "mm_page_alloc", "mm_page_alloc_zone_locked":
 			pfn_tracker.entries = append(pfn_tracker.entries, entry)
 			pfn_tracker.pfnmap[entry.pfn] = entry
 			pfn_tracker.alloc_bytes += order_to_bytes(entry.order)
-		case "mm_page_alloc_zone_locked":
+		case "mm_page_free", "mm_page_free_batched":
 			pfn_tracker.entries = append(pfn_tracker.entries, entry)
-			pfn_tracker.pfnmap[entry.pfn] = entry
-			pfn_tracker.alloc_bytes += order_to_bytes(entry.order)
-		case "mm_page_free":
-			pfn_tracker.entries = append(pfn_tracker.entries, entry)
-			pfn_tracker.free_bytes += order_to_bytes(entry.order)
-		case "mm_page_free_batched":
-			pfn_tracker.entries = append(pfn_tracker.entries, entry)
-			pfn_tracker.free_bytes += order_to_bytes(entry.order)
+			if pfn_tracker.pfnmap[entry.pfn] != nil {
+				pfn_tracker.free_bytes += order_to_bytes(entry.order)
+				pfn_tracker.pfnmap[entry.pfn] = nil
+			}
+		case "kmem_cache_alloc", "kmalloc_node", "kmalloc":
+			kmem_tracker.entries = append(kmem_tracker.entries, entry)
+			kmem_tracker.alloc_bytes += entry.bytes_allocated
+			kmem_tracker.kmemmap[entry.ptr] = entry
+		case "kmem_cache_free", "kfree":
+			kmem_tracker.entries = append(kmem_tracker.entries, entry)
+			if kmem_tracker.kmemmap[entry.ptr] != nil {
+				kmem_tracker.free_bytes += kmem_tracker.kmemmap[entry.ptr].bytes_allocated
+				kmem_tracker.kmemmap[entry.ptr] = nil
+			}
 		}
 	}
-	fmt.Printf("alloc bytes = %d = %d Kbytes = %d Mbytes\n", pfn_tracker.alloc_bytes,
-		  pfn_tracker.alloc_bytes / 1024, pfn_tracker.alloc_bytes / (1024 * 1024))
-	fmt.Printf("free bytes = %d = %d Kbytes = %d Mbytes\n", pfn_tracker.free_bytes,
-		  pfn_tracker.free_bytes / 1024, pfn_tracker.free_bytes / (1024 * 1024))
+	fmt.Printf("page alloc bytes = %d = %d Kbytes = %d Mbytes\n", pfn_tracker.alloc_bytes,
+		pfn_tracker.alloc_bytes/1024, pfn_tracker.alloc_bytes/(1024*1024))
+	fmt.Printf("page free bytes = %d = %d Kbytes = %d Mbytes\n", pfn_tracker.free_bytes,
+		pfn_tracker.free_bytes/1024, pfn_tracker.free_bytes/(1024*1024))
+	fmt.Printf("kmem alloc bytes = %d = %d Kbytes = %d Mbytes\n", kmem_tracker.alloc_bytes,
+		kmem_tracker.alloc_bytes/1024, kmem_tracker.alloc_bytes/(1024*1024))
+	fmt.Printf("kmem free bytes = %d = %d Kbytes = %d Mbytes\n", kmem_tracker.free_bytes,
+		kmem_tracker.free_bytes/1024, kmem_tracker.free_bytes/(1024*1024))
 }
