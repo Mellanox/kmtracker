c6fb917a3b341782143edbf6a05fbcc2040cacd8
Parav Pandit <parav@nvidia.com>
2020-11-27 09:06:01 +0200

    scripts: Stop the trace before reading it
    Stop the trace before reading the trace contents.
    
    Signed-off-by: Parav Pandit <parav@nvidia.com>

--- scripts/stop_trace.sh
+++ scripts/stop_trace.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cat /sys/kernel/debug/tracing/trace > trace.txt
 echo 0 > /sys/kernel/debug/tracing/tracing_on
+cat /sys/kernel/debug/tracing/trace > trace.txt
 echo  > /sys/kernel/debug/tracing/trace
